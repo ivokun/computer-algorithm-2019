35ddd8d0ee21437e0c56c3a4a55b39f7fc23870f
ivokun <salahuddin.mi@gmail.com>
2019-03-20 12:29:08 +0900

    update zombies position

--- zombie.py
+++ zombie.py
@@ -1,52 +1,16 @@
 import random
 
-zombies = 1
-bridges_count = 10
-position = [4]
+print("="*50)
+print("Walking Dead")
+print("="*50)
+zombies = int(input("Zombies = "))
+bridge_length = int(input("Bridge length = "))
+position = input("Put initial position = ").split()
 zombies_out = 0
 time = 0
-bridges = []
+bridge = []
 
-def print_bridge(count):
-  for i in range(count):
-    print(i, sep='', end=' ', flush=True)
+for position in range(bridge_length):
+  bridge.append(0)
 
-def zombie_position(bridges):
-  for bridge_position in range(len(bridges)):
-    if bridges[bridge_position][0] == "zombie":
-      if bridges[bridge_position][1] > 0:
-        print('<', end=' ')
-      else:
-        print('>', end=' ')
-    else:
-      print(' ', end=' ')
-    
-
-for i in range(bridges_count):
-  if i in position:
-    bridges.append(["zombie", (-1)**((random.randrange(2)))])
-  else:
-    bridges.append([0, 0])
-while zombies:
-  time+=1
-  print_bridge(bridges_count)
-  print(' (time = {} sec)'.format(time))
-  for current_position in range(len(bridges)):
-    if bridges[current_position][0] == "zombie":
-      if bridges[current_position][1] > 0:
-        print('<', end=' ')
-      else:
-        print('>', end=' ')
-      next_position = current_position - bridges[current_position][1]
-      if current_position - bridges[current_position][1] == 0 or current_position - bridges[current_position][1] == len(bridges) - 1:
-        bridges[current_position] = [0,0]
-        zombies -= 1
-      elif bridges[next_position][0] == "zombie":
-        bridges[current_position][1] *= -1
-      else:
-        bridges[next_position] = bridges[current_position]
-        bridges[current_position] = [0,0] 
-    else:
-      print(' ', end=' ')
-  print('')
-  # zombies = 0
+print(bridge)
