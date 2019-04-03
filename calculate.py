f0076668c3c5898a6ba45b6da1bbad59230e4c99
ivokun <salahuddin.mi@gmail.com>
2019-04-03 23:24:01 +0900

    Fix calculate sum

--- calculate.py
+++ calculate.py
@@ -1,1 +1,27 @@
-input = input("Integers= ")
+p = 1
+i = 1
+while 1:
+  try:
+    raw_num = input("Integer = ")
+    num = int(raw_num)
+    if num < 0:
+      print("Please input more then zero!")
+      exit()
+    if num > 10000:
+      print("Please input below 10000!")
+      exit()
+    if num % 2 == 0:
+      print("Please input not divisible by 2")
+      exit()
+    elif num % 5 == 0:
+      print("Please input not divisible by 5")
+      exit()
+    else:
+      while p % num != 0:
+        p += (10 ** i)
+        i += 1
+      print(i)
+      exit()
+  except ValueError:
+    print("Input is not integer!")
+    exit()
