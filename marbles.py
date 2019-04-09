34160edf275a8406b99226836b26bf3a8e0a25e7
ivokun <salahuddin.mi@gmail.com>
2019-04-09 19:09:30 +0900

    change to numpy matrix

--- marbles.py
+++ marbles.py
@@ -1,33 +1,13 @@
+import numpy as np
 marblesCount = 2000000000
 max = 2
 
-def multiply(F, M):
-  x =  F[0][0] * M[0][0] + F[0][1] * M[1][0]
-  y =  F[0][0] * M[0][1] + F[0][1] * M[1][1]
-  z =  F[1][0] * M[0][0] + F[1][1] * M[1][0]
-  w =  F[1][0] * M[0][1] + F[1][1] * M[1][1]
-  F[0][0] = x
-  F[0][1] = y
-  F[1][0] = z
-  F[1][1] = w
+def fibo_mx(n):
+  x = np.matrix([[1,1],[1,0]])
+  b = x**n
+  return b
 
-def power(F, n):
-  if n == 0 or n == 1:
-    return
-  M = [[1,1],[1,0]]
-  power(F, n / 2)
-  multiply(F, F)
-  if n % 2 != 0:
-    multiply(F, M)
-
-def fibo_matrix(n):
-  F = [[1,1],[1,0]]
-  if n == 0:
-    return 0
-  power(F, n - 1)
-  return F[0][0]
-
-print(fibo_matrix(8) % 65535)
+print(fibo_mx(marblesCount))
 
 
 # def pickMarbles(marbles, maxPick):
